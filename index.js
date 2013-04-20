945900bb68c36f06e63052756830fe7f6c9ee145
PockyTamer <tsaok@uw.edu>
2013-04-19 22:52:09 -0700

    Still testing click.

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 // index.js contains the ui for managing the schedule
 // as well as ajax requests for server data.
 
-$(document).ready(function() {
+/*$(document).ready(function() {
     alert("test");
-    $("body").mousedown(function() {
-        alert("You clicked down!");
-    })
-});
+    //$("body").click(function() {
+      //  alert("You clicked down!");
+    //});
+    var body = document.getElementById("body");
+    body.onmousedown = makeDiv;
+});*/
+
+(function() {
+    window.onlaod = function() {
+      var body = document.getelementById("body");
+      body.onclick = success;
+    };
+    
+    function success() {
+        alert("Success!");
+    }
+}) ();
