6b00600fe443a16f590f086655442567972d50ac
PockyTamer <tsaok@uw.edu>
2013-04-19 23:16:39 -0700

    Getting correct coordinates for div

--- index.js
+++ index.js
@@ -2,14 +2,27 @@
 
 // index.js contains the ui for managing the schedule
 // as well as ajax requests for server data.
+var x = 0;
+var y = 0;
 
 $(document).ready(function() {
+
     alert("test");
-    //$("body").click(function() {
-      //  alert("You clicked down!");
-    //});
-    $("tbody").mousedown(function() {
+
+    $("tbody").mousedown(function(e) {
         alert("clicked");
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+    });
+    
+    $("tbody").mouseup(function(e) {
+       alert("release");
+       var x -= e.pageX - this.offsetLeft;
+       var y -= e.pageY - this.offsetTop;
+       x = Math.abs(x);
+       y = Math.abs(y);
+       alert(x);
+       alert(y);
     });
 });
 
