da618425b16ee5b8dc1ea8672dc1115a982a3ccc
MaryHal <maryhal@users.noreply.github.com>
2013-04-19 20:37:57 -0700

    Pretty printing JSON in PHP

--- printJson.php
+++ printJson.php
@@ -3,7 +3,7 @@
 
 $data = get_data("http://search.twitter.com/search.json?q=hello");
 header('Content-type: text/javascript');
-echo json_encode($data);
+echo prettyPrint($data);
 
 function get_data($url)
 {
@@ -15,6 +15,67 @@ function get_data($url)
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
+}
+
+function prettyPrint($json)
+{
+    $result = '';
+    $level = 0;
+    $prev_char = '';
+    $in_quotes = false;
+    $ends_line_level = NULL;
+    $json_length = strlen($json);
+
+    for($i = 0; $i < $json_length; $i++)
+    {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if($ends_line_level !== NULL)
+        {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if($char === '"' && $prev_char != '\\')
+        {
+            $in_quotes = !$in_quotes;
+        }
+        else if(! $in_quotes)
+        {
+            switch($char)
+            {
+            case '}': case ']':
+                $level--;
+                $ends_line_level = NULL;
+                $new_line_level = $level;
+                break;
+
+            case '{': case '[':
+                $level++;
+            case ',':
+                $ends_line_level = $level;
+                break;
+
+            case ':':
+                $post = " ";
+                break;
+
+            case " ": case "\t": case "\n": case "\r":
+                $char = "";
+                $ends_line_level = $new_line_level;
+                $new_line_level = NULL;
+                break;
+            }
+        }
+        if($new_line_level !== NULL)
+        {
+            $result .= "\n".str_repeat( "\t", $new_line_level );
+        }
+        $result .= $char.$post;
+        $prev_char = $char;
+    }
+
+    return $result;
 }
 
 ?>
